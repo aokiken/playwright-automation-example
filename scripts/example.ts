f3188b233a56a6461bd4fe0e1a39c3e0ce5dcc7a
aokiken <aokiken@me.com>
2022-09-20 16:50:35 +0900

    Add scripts/example.ts

---
+++ scripts/example.ts
@@ -0,0 +1,9 @@
+const { chromium, firefox, webkit } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch();  // Or 'firefox' or 'webkit'.
+  const page = await browser.newPage();
+  await page.goto('http://example.com');
+  // other actions...
+  await browser.close();
+})();
